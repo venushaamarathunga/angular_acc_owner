c3dc8e5446c8fbd4247441d9f9760a0213cbc75b
venusha <venusha@192.168.8.100>
2023-06-25 18:47:53 +0530

    direactives added

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ErrorModalComponent } from './modals/error-modal/error-modal.component';
 import { ModalModule } from 'ngx-bootstrap/modal';
+import { AppendDirective } from './directives/append.directive';
 
 @NgModule({
-  declarations: [ErrorModalComponent],
+  declarations: [ErrorModalComponent, AppendDirective],
   imports: [CommonModule], // ModalModule.forChild()],
-  exports: [ErrorModalComponent],
+  exports: [ErrorModalComponent, AppendDirective],
 })
 export class SharedModule {}
