f77a0cc806d1d7b0d4a0fc8b19be08c62c963263
venushaamarathunga <avenusha95@gmail.com>
2023-06-25 19:21:46 +0530

    error or the merge in derectives branch

--- src/app/shared/directives/append.directive.ts
+++ src/app/shared/directives/append.directive.ts
@@ -1,10 +1,29 @@
-import { Directive } from '@angular/core';
+import {
+  Directive,
+  ElementRef,
+  Input,
+  OnChanges,
+  Renderer2,
+  SimpleChanges,
+} from '@angular/core';
+import { Owner } from '../../_interfaces/owner.model';
 
 @Directive({
-  selector: '[appAppend]'
+  selector: '[appAppend]',
 })
-export class AppendDirective {
+export class AppendDirective implements OnChanges {
+  @Input('appAppend') ownerParam: Owner | undefined;
 
-  constructor() { }
+  constructor(private element: ElementRef, private renderer: Renderer2) {}
 
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['ownerParam'].currentValue) {
+      const accNum = changesgit['ownerParam'].currentValue.accounts.length;
+      const span = this.renderer.createElement('span');
+      const text = this.renderer.createText(` (${accNum}) accounts`);
+
+      this.renderer.appendChild(span, text);
+      this.renderer.appendChild(this.element.nativeElement, span);
+    }
+  }
 }
